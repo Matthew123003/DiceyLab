491489edee324ceeb69a80cccf7b1e452d84be4f
Matt Kramer <matthew123003@gmail.com>
2024-03-09 12:42:33 -0500

    Started

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -1,5 +1,27 @@
 public class Simulation {
+        private int numberOfDies;
+        private int numberOfTosses;
 
+    public Simulation(int numberOfDies, int numberOfTosses){
+        this.numberOfDies = numberOfDies;
+        this.numberOfTosses = numberOfTosses;
+    }
+
+    public int getNumberOfDies() {
+        return numberOfDies;
+    }
+
+    public void setNumberOfDies(int numberOfDies) {
+        this.numberOfDies = numberOfDies;
+    }
+
+    public int getNumberOfTosses() {
+        return numberOfTosses;
+    }
+
+    public void setNumberOfTosses(int numberOfTosses) {
+        this.numberOfTosses = numberOfTosses;
+    }
 
 
 }
