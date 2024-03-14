1c48ec8a1aee0a0c81e069bd4c503ad1ce165fd6
Matt Kramer <matthew123003@gmail.com>
2024-03-14 14:50:09 -0400

    Finished

--- src/main/java/Simulation.java
+++ src/main/java/Simulation.java
@@ -36,19 +36,29 @@ public class Simulation {
         }
     }
 
-        public void printResults () {
+        public String printResults () {
             StringBuilder sb = new StringBuilder();
             sb.append("***\n");
             sb.append("Simulation of ").append(numberOfDies).append(" dice tossed for ").append(numberOfTosses).append(" times.\n");
             sb.append("***\n");
             sb.append("\n");
-            for()
-
+            for(int i = numberOfDies; i <= 6*numberOfDies; i++){
+                sb.append(String.format("%2d : %8d : %.2f", i, bins.getBin(i), (double)bins.getBin(i) / numberOfTosses));
+                for(int j = 0; j < (double) bins.getBin(i)/ numberOfTosses * 100; j++){
+                    sb.append("*");
+                }
+                sb.append("\n");
+            }
+            return sb.toString();
         }
 
         //Simulation of 2 dice tossed for 1000000 times.
     public static void main(String[] args){
+            Simulation sim = new Simulation(2,1000000, 2, 12);
 
+            sim.runSimulation();
+
+        System.out.println(sim.printResults());
 
     }
     }
